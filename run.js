5178ecf95c351a471bd32ba57be1a8df7575ff91
= <bradmedeiros0@gmail.com>
2017-02-19 22:54:37 -0800

    -

--- run.js
+++ run.js
@@ -1,16 +1,16 @@
 
 var path = require("path");
 
-console.log(path.normalize("."));
+//console.log(path.normalize("."));
 //consle.log(path.resolve(path.dirname(),'./fs_mount_mqtt.js'));
-mq = require(path.resolve(path.dirname(),'./fs_mount_mqtt.js'));
+mq = require('./fs_mount_mqtt.js');
 
 var should_run = (process.argv[2] == "yes");
 
 console.log(process.argv);
 root = path.resolve("./mock");
 root1 = path.resolve("./mock0");
-root_shared = path.resolve("../../run/mock");
+root_shared = path.resolve("./mock");
 var the_extensions = {
     "/actions/": ".action.json",
     "/states/":  ".state.json"
